5ba14088a71ad158967e3b80b9029dd71705f8bc
Rajat <rs20021023@gmail.com>
2022-04-15 19:31:42 +0530

    initial commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,10 @@
+import React from "react";
+export default function Navbar(props) {
+  return (
+    <div className="navbar">
+      <a href="#" className="brand">
+        {props.brand}
+      </a>
+      </div>
+  );
+}

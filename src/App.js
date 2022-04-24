5edbb98780e81dae230eda462cb74414664802a9
Rajat <rs20021023@gmail.com>
2022-04-24 11:43:54 +0530

    added new level and can start new game after gameover

--- src/App.js
+++ src/App.js
@@ -4,16 +4,15 @@ import Rules from "./components/Rules";
 import Score from "./components/Score";
 import Card from "./components/Card";
 import { useState } from "react";
-import NextLevel from "./components/NextLevel";
 import GameOver from "./components/GameOver";
 
 function App() {
   const [current, setCurrent] = useState(0);
   const [high, setHigh] = useState(0);
-  const [nextLevel, setNextLevel] = useState(false);
   const [gameOver, setGameOver] = useState(false);
   const [cardRender, setCardRender] = useState(false);
   const [checkArray, setCheckArray] = useState([]);
+  const [levelNum, setLevelNum] = useState(1);
   const pokemonArray = [
     "pokemon1",
     "pokemon2",
@@ -27,34 +26,46 @@ function App() {
     "pokemon10",
     "pokemon11",
     "pokemon12",
+    "pokemon13",
+    "pokemon14",
+    "pokemon15",
+    "pokemon16",
+    "pokemon17",
+    "pokemon18",
+    "pokemon19",
+    "pokemon20",
   ];
 
   const shuffle = (startArray) => {
-    for (let i = startArray.length - 1; i > 0; i--) {
+    let tempArray = startArray;
+    for (let i = tempArray.length - 1; i > 0; i--) {
       let j = Math.floor(Math.random() * (i + 1));
-      [startArray[i], startArray[j]] = [startArray[j], startArray[i]];
+      [tempArray[i], tempArray[j]] = [tempArray[j], tempArray[i]];
     }
-    setStartArray(startArray);
+
+    if (tempArray !== startArray) {
+      shuffle(startArray);
+    } else {
+      setStartArray(tempArray);
+    }
   };
 
   const startGame = () => {
-    console.log("IN start game");
-    const range1 = Math.floor(Math.random() * 8) + 1;
+    const range1 = Math.floor(Math.random() * 16) + 1;
     const range2 = range1 + 4;
     const startArray = pokemonArray.slice(range1, range2);
     return startArray;
   };
 
   const levelArrayMaker = () => {
-    const range1 = Math.floor(Math.random() * 10) + 1;
+    const range1 = Math.floor(Math.random() * 18) + 1;
     const range2 = range1 + 2;
     const newArray = pokemonArray.slice(range1, range2);
     return newArray;
   };
 
-  const startNextLevel = (prevLevelArray) => {
+  const startNextLevel = (prevLevelArray, level) => {
     let newArray = levelArrayMaker();
-
     while (
       prevLevelArray.includes(newArray[0]) ||
       prevLevelArray.includes(newArray[1])
@@ -62,19 +73,29 @@ function App() {
       newArray = levelArrayMaker();
     }
     setStartArray([...startArray, ...newArray]);
+    setLevelNum(level);
+    setCheckArray([]);
+  };
+
+  const startNewGame = () => {
+    if (current > high) setHigh(current);
+    setCurrent(0);
+    setCheckArray([]);
+    setStartArray(startGame());
+    setLevelNum(1);
+    setGameOver(!gameOver);
   };
 
   const checkWin = (e, startArray) => {
     const element = e.target.getAttribute("name");
-    console.log(startArray);
-    if (checkArray.includes(element)) {
-      setGameOver(!gameOver);
-    } else if (checkArray.length === 3) {
-      // setNextLevel(!nextLevel);
-      startNextLevel(startArray);
+
+    if (checkArray.includes(element)) setGameOver(!gameOver);
+    else if (checkArray.length === startArray.length - 1) {
+      startNextLevel(startArray, levelNum + 1);
       setCardRender(!cardRender);
     } else {
       setCheckArray([...checkArray, element]);
+      setCurrent((prev) => prev + 1);
       setCardRender(!cardRender);
       shuffle(startArray);
     }
@@ -83,38 +104,24 @@ function App() {
   const arr = startGame();
   const [startArray, setStartArray] = useState(arr);
 
-  if (nextLevel) {
+
+  if (gameOver) {
     return (
       <div>
-        <Navbar brand={"Memory-game"} />
-        <NextLevel level={1} />
-      </div>
-    );
-  } else if (gameOver) {
-    return (
-      <div>
-        <Navbar brand={"Memory-game"} />
-        <GameOver />
+        <Navbar level={levelNum} brand={"Memory-game"} />
+        <GameOver startNewGame={startNewGame} />
       </div>
     );
   } else {
     return (
       <div>
-        <Navbar brand={"Memory-game"} />
+        <Navbar level={levelNum} brand={"Memory-game"} />
         <Rules />
         <Score current={current} high={high} />
         <Card startArray={startArray} checkWin={checkWin} />
       </div>
     );
   }
-  // return (
-  //   <div>
-  //     <Navbar brand={"Memory-game"} />
-  //     <Rules />
-  //     <Score current={current} high={high} />
-  //     <Card startArray={startArray} checkWin={checkWin} />
-  //   </div>
-  // );
 }
 
 export default App;
--- src/components/GameOver.js
+++ src/components/GameOver.js
@@ -1,11 +1,11 @@
 import React from "react";
 
-function Gameove() {
+function Gameove(props) {
   return (
     <div className="center">
       <h1>Game over !!!!</h1>
       <div className="btn-container">
-        <button className="gameover-button">Play again</button>
+        <button onClick={props.startNewGame} className="gameover-button">Play again</button>
       </div>
     </div>
   );
